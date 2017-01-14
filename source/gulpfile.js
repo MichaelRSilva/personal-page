6eb10369ee35de9fc7aeabdd16bac18bbd803a78
Michael Rodrigues <michaeldf.ti@gmail.com>
2017-01-14 16:22:39 -0200

    adding compress image to .gulpfile

--- source/gulpfile.js
+++ source/gulpfile.js
@@ -2,20 +2,23 @@
  * Created by michaeldfti on 14/01/17.
  */
 
-var gulp            = require('gulp');
-var livereload      = require("gulp-livereload");
-var sass            = require("gulp-sass");
-var sourcemaps      = require("gulp-sourcemaps");
-var uglify          = require("gulp-uglify");
-var concat          = require("gulp-concat");
-var minifyCss       = require("gulp-minify-css");
-var plumber         = require("gulp-plumber");
+var gulp                    = require('gulp');
+var livereload              = require("gulp-livereload");
+var sass                    = require("gulp-sass");
+var sourcemaps              = require("gulp-sourcemaps");
+var uglify                  = require("gulp-uglify");
+var concat                  = require("gulp-concat");
+var plumber                 = require("gulp-plumber");
+var imagemin                = require('gulp-imagemin');
+var imageminPngquant        = require('imagemin-pngquant');
+var imageminJpegRecompress  = require('imagemin-jpeg-recompress');
 
 
 //Filespaths
 var DIST_PATH    = 'public/dist';
 var SCRIPTS_PATH = 'public/scripts/**/*.js';
 var SCSS_PATH    = 'public/css/**/*.scss';
+var IMAGES_PATH  = 'public/images/**/*.{png,jpeg,jpg,svg,gif}';
 
 //Styles For SCSS
 gulp.task("styles", function(){
@@ -55,6 +58,19 @@ gulp.task("scripts", function(){
 //Images
 gulp.task("images", function(){
     console.log("starting images task");
+    return gulp.src(IMAGES_PATH)
+        .pipe(imagemin(
+            [
+                imagemin.gifsicle(),
+                imagemin.jpegtran(),
+                imagemin.optipng(),
+                imagemin.svgo(),
+                imageminPngquant(),
+                imageminJpegRecompress()
+            ]
+        ))
+        .pipe(gulp.dest(DIST_PATH + '/images'))
+        .pipe(livereload());
 });
 
 
@@ -68,7 +84,7 @@ gulp.task('watch', ['default'], function () {
     console.log("starting watch task");
     require("./server.js");
     livereload.listen();
-
+    gulp.watch(IMAGES_PATH, ['images']);
     gulp.watch(SCRIPTS_PATH, ['scripts']);
     gulp.watch(SCSS_PATH, ['styles']);
 
